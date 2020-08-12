6d5ee5668aff02b8e98f0f5ac61f8a7d31d9ce36
jundic <jundi_chen@sfu.ca>
2020-08-12 09:38:49 -0700

    added heroku database connection

--- config/connectToDatabase.php
+++ config/connectToDatabase.php
@@ -6,10 +6,17 @@
 
 date_default_timezone_set("America/Vancouver");
 
-$serverName = "127.0.0.1";
-$userName = "root";
-$password = ""; // no password for now;
-$databaseName = "Aj";
+//$serverName = "127.0.0.1";
+//$userName = "root";
+//$password = ""; // no password for now;
+//$databaseName = "Aj";
+    
+// connect to heroku info
+//mysql://bcd714d605f1f9:c6d8bcd8@us-cdbr-east-02.cleardb.com/heroku_cee4c026af3fbf5?reconnect=true
+$serverName = "us-cdbr-east-02.cleardb.com";
+$userName = "bcd714d605f1f9";
+$password = "c6d8bcd8";
+$databaseName = "heroku_cee4c026af3fbf5";
 
 /* check connection */
 if (mysqli_connect_errno()) {
